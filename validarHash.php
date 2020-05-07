a15054b1bf1f96dcb8c0dcb13cad98f85a778ae5
andresmoreno95 <37678726+andresmoreno95@users.noreply.github.com>
2020-05-07 17:37:01 -0500

    Add files via upload

---
+++ validarHash.php
@@ -0,0 +1,14 @@
+
+<? php
+requiere  "validarHash.php" ;
+
+if ( isset ( $ _POST [ "h1" ], $ _POST [ "h2" ], $ _POST [ "dinero" ])) {
+
+    $ validador = new validarHash ( "" , "" , "" );
+    $ resp = $ validador -> validarInfo ( $ _POST [ "h1" ], $ _POST [ "h2" ], $ _POST [ "dinero" ]);
+
+    echo  json_encode ( $ resp );
+}
+
+
+?>
